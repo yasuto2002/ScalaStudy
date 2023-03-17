d7de702faddc2907db304b16ff873eff8c145a7f
yasuto <fujiya0101@gmail.com>
2023-03-17 22:12:00 +0900

    add:3/14

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,5 +1,20 @@
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    val p1 = new BPrinter
+    p1.print()
   }
-}
+
+  class APrinter {
+    def print(): Unit = {
+      println(1)
+    }
+  }
+
+  class BPrinter extends APrinter {
+    override def print(): Unit = {
+      println(2)
+    }
+  }
+
+  }
--- build.sbt
+++ build.sbt
@@ -2,3 +2,4 @@ scalaVersion := "2.12.4"
 
 scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-Xlint")
 Global / serverConnectionType := ConnectionType.Tcp // experimental
+Global / cancelable := false // ctrl-c
